77acd286faf6b344a8283bff295d10eb029581df
Alex Orga <alexorga.dev@gmail.com>
2018-09-10 18:37:11 -0500

    printing squared numbers from ranNumList

--- SquaredRandoms/Program.cs
+++ SquaredRandoms/Program.cs
@@ -8,23 +8,7 @@ namespace SquaredRandoms
         static void Main(string[] args)
         {
             var ranNumList = new RanNumList();
-            RanNumList randomNumbersList = ranNumList.GenerateRanNumList();
-            RanNumListSquared
-            //Random random = new Random();
-
-            //List<int> ranNumList = new List<int>();
-            //List<int> ranNumListSquared = new List<int>();
-
-            ////for (int i = 0; i < 20; i++)
-            //{
-            //    ranNumList.Add(random.Next(50));
-            //}
-
-            //foreach(int ranNum in ranNumList)
-            //{
-            //    int ranNumSquared = ranNum * ranNum;
-            //    ranNumListSquared.Add(ranNumSquared);
-            //}
+            ranNumList.GenerateAndPrintRanNumList();
 
             Console.ReadLine();
         }
--- SquaredRandoms/ranNumList.cs
+++ SquaredRandoms/ranNumList.cs
@@ -8,8 +8,9 @@ namespace SquaredRandoms
     {
         Random random = new Random();
         List<int> ranNumList = new List<int>();
+        List<int> ranNumListSq = new List<int>();
 
-        public void GenerateRanNumList()
+        private void GenerateRanNumList()
         {
             for(int i = 0; i < 20; i++)
             {
@@ -17,18 +18,53 @@ namespace SquaredRandoms
             }
         }
 
-        //private void PrintRanNumList()
-        //{
-        //    foreach(int ranNum in ranNumList)
-        //    {
-        //        Console.WriteLine($"ranNum: {ranNum}");
-        //    }
-        //}
+        private void PrintRanNumList()
+        {
+            foreach(int ranNum in ranNumList)
+            {
+                Console.WriteLine($"ranNum: {ranNum}");
+            }
+        }
+
+        private void GenerateSquaredNumList()
+        {
+            foreach(int ranNum in ranNumList)
+            {
+                var itemSquared = ranNum * ranNum;
+                ranNumListSq.Add(itemSquared);
+            }
+        }
+
+        private void PrintSquaredNumList()
+        {
+            foreach(int item in ranNumListSq)
+            {
+                Console.WriteLine($"squaredNum: {item}");
+            }
+        }
+
+        private void RemoveOddNumbersFromSquaredList()
+        {
+            foreach(int item in ranNumListSq)
+            {
+                if ((item % 2) != 0)
+                {
+                    ranNumListSq.Remove(item);
+                }
+                else
+                {
+                    Console.WriteLine($"notOdd: {item}");
+                }
+            }
+        }
 
         public void GenerateAndPrintRanNumList()
         {
             GenerateRanNumList();
-            //PrintRanNumList();
+            PrintRanNumList();
+            GenerateSquaredNumList();
+            PrintSquaredNumList();
+            RemoveOddNumbersFromSquaredList();
         }
     }
 }
